fe58c3006205de1149db7334f6bdba47a98e0628
Raihan <ataul.raihan@ideascale.com>
2023-09-01 18:20:06 +0600

    Multi thread activated

--- src/main/java/com/raihanorium/javaconcurrency/processor/PhysicalStorageFileProcessingStrategy.java
+++ src/main/java/com/raihanorium/javaconcurrency/processor/PhysicalStorageFileProcessingStrategy.java
@@ -2,6 +2,7 @@ package com.raihanorium.javaconcurrency.processor;
 
 import com.raihanorium.javaconcurrency.constants.Constants;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.util.concurrent.ExecutorService;
@@ -14,15 +15,8 @@ public class PhysicalStorageFileProcessingStrategy implements FileProcessingStra
     public boolean process(String fileName) {
         try {
             File root = new File(Constants.PHYSICAL_STORAGE_DIRECTORY);
-            if (root.exists()) {
-                if (!root.delete()) {
-                    throw new RuntimeException("Could not clear existing physical storage directory.");
-                }
-            }
-
-            if (!root.mkdir()) {
-                throw new RuntimeException("Could not create physical storage directory.");
-            }
+            FileUtils.forceMkdir(root);
+            FileUtils.cleanDirectory(root);
 
             ExecutorService executorService = Executors.newFixedThreadPool(Constants.THREAD_POOL_SIZE);
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
@@ -32,54 +26,26 @@ public class PhysicalStorageFileProcessingStrategy implements FileProcessingStra
                 final String content = line;
                 final long index = count;
                 executorService.submit(() -> {
-                    File vcfFile = new File(getDirectory(root, index), String.valueOf(index));
-                    if (!vcfFile.createNewFile()) {
-                        log.error("Could not create vcf file {}", index);
-                    }
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(getDirectory(root, index), "contact" + index + ".vcf"), false))) {
+                    File directory = new File(root, String.valueOf(index / 1000));
+                    FileUtils.forceMkdir(directory);
+                    File vcfFile = new File(directory, "contact" + index + ".vcf");
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(vcfFile, false))) {
+                        log.info("Writing file {} in thread {}", vcfFile.getName(), Thread.currentThread().getName());
                         writer.write(content);
+                    } catch (Exception ex) {
+                        log.error(ex.getMessage(), ex);
                     }
                     return vcfFile;
                 });
-                executorService.shutdown();
-                if (executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
-                    throw new RuntimeException("Executor service terminated.");
-                }
                 count++;
+            }
+            executorService.shutdown();
+            if (executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
+                executorService.shutdownNow();
             }
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
         return true;
-    }
-
-    private File getDirectory(File rootDirectory, long currentFileNumber) throws IOException {
-        long firstLayerIndex = currentFileNumber / 1000;
-
-        if (currentFileNumber < 1000) {
-            File file = new File(rootDirectory, String.valueOf(currentFileNumber / 1000));
-            if (!file.exists()) {
-                if (!file.createNewFile()) {
-                    throw new RuntimeException("Could not create directory");
-                }
-            }
-            return file;
-        } else if (currentFileNumber < 1000000) {
-            File layer1Directory = new File(rootDirectory, String.valueOf(currentFileNumber / 1000));
-            if (!layer1Directory.exists()) {
-                if (!layer1Directory.createNewFile()) {
-                    throw new RuntimeException("Could not create directory");
-                }
-            }
-
-            File file = new File(layer1Directory, String.valueOf(currentFileNumber / 100000));
-            if (!file.exists()) {
-                if (!file.createNewFile()) {
-                    throw new RuntimeException("Could not create directory");
-                }
-            }
-            return file;
-        }
-        return null;
     }
 }
